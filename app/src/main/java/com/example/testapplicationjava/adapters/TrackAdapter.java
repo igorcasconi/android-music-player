fdecd9ed473ddd7137fba75003097c245830474a
Igor Casconi <igor.oliveira@nave.rs>
2022-07-25 20:15:56 -0300

    feat(home-activity): on click item listener added

--- app/src/main/java/com/example/testapplicationjava/adapters/TrackAdapter.java
+++ app/src/main/java/com/example/testapplicationjava/adapters/TrackAdapter.java
@@ -1,18 +1,17 @@
 package com.example.testapplicationjava.adapters;
 
 import android.content.Context;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.testapplicationjava.R;
 import com.example.testapplicationjava.models.AlbumOfTrack;
-import com.example.testapplicationjava.models.ProfileArtist;
 import com.example.testapplicationjava.models.TrackDataItem;
 import com.example.testapplicationjava.models.TrackData;
 import com.example.testapplicationjava.utils.DownloadImageTask;
@@ -21,7 +20,6 @@ import com.example.testapplicationjava.utils.TimeString;
 import java.util.List;
 
 public class TrackAdapter extends RecyclerView.Adapter<TrackAdapter.TrackViewHolder> {
-
   private List<TrackData> trackList;
   private Context context;
   private TimeString timeString = new TimeString();
@@ -40,6 +38,7 @@ public class TrackAdapter extends RecyclerView.Adapter<TrackAdapter.TrackViewHol
     LayoutInflater inflater = LayoutInflater.from(parent.getContext());
     View itemTrackList = inflater.inflate(R.layout.track_item_list, parent, false);
     TrackViewHolder viewHolder = new TrackViewHolder(itemTrackList);
+
     return viewHolder;
   }
 
@@ -73,13 +72,14 @@ public class TrackAdapter extends RecyclerView.Adapter<TrackAdapter.TrackViewHol
     private ImageView songImage;
     private TextView duration;
 
-    public TrackViewHolder(View itemView) {
+    public TrackViewHolder(@NonNull View itemView) {
       super(itemView);
 
       songName = itemView.findViewById(R.id.songNameItem);
       artist = itemView.findViewById(R.id.artistNameItem);
       songImage = itemView.findViewById(R.id.songImageItem);
       duration = itemView.findViewById(R.id.songDurationItem);
+
     }
   }
 }
